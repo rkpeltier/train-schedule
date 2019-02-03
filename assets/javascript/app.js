1fcfcdb77c1a7938c28c9f932ef1e81ddf7233c4
rkpeltier <rkpeltier@gmail.com>
2019-02-02 19:07:53 -0800

    added functionality for adding train info to table and calculating minutes away (which is wrong right now, but it's doing a thing)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,32 +10,48 @@ var config = {
   firebase.initializeApp(config);
 
   //Database
-  var database = firebase.Database();
+  var database = firebase.database();
 
   //Add values to db
   $("#add-train").on("click", function(event) {
     event.preventDefault();
 
-    var trainName = $("#train-name-input").val().trim();
+    var name = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var time = $("#train-time-input").val().trim();
     var frequency = $("#train-frequency-input").val().trim();
 
     database.ref().push({
-        name: trainName,
+        name: name,
         destination: destination,
         time: time,
         freqency: frequency
     });
 
-    // database.ref().on("value", function(){
-    //     var dbObj = database.val();
-    //     var moment = moment(dbObj.time,"HH:mm");
+    //Adding to the table
+  database.ref().on("child_added", function(db){
+    //DB Object variable
+      var dbObj = db.val();
+  //Converting to momentjs object
+      var trainTime = moment(dbObj.time,"HH:mm").subtract(1, "years");
+      var trainFreq = moment(dbObj.freqency, "mm");
+      var diffTime = moment().diff(trainTime, "minutes");
+    //The Math
+      var remainingTime = diffTime % trainFreq;
+      var minAway = remainingTime - trainFreq;
+    
 
-    // })
+
+
+      var newRow = $("<tr>")
+      newRow.append($('<td>' + dbObj.name + '</td>'));
+      newRow.append($('<td>' + dbObj.destination + '</td>'));
+      newRow.append($('<td>' + dbObj.freqency + '</td>'));
+      newRow.append($('<td>' + dbObj.time + '</td>'));
+      newRow.append($('<td>' + minAway + '</td>'));
+
+      $('tbody').append(newRow);
 
   });
-
-  //Calculating freq with moment JS.
-  //convert the time and then diff the two in min.
-  // %
+    
+  });
