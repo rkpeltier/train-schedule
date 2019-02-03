8bdf1e378f88eea61a421d869c5006844806e459
rkpeltier <rkpeltier@gmail.com>
2019-02-02 17:21:53 -0800

    added submit button and updatd js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,29 @@ var config = {
   //Database
   var database = firebase.Database();
 
-  //Code for App below
+  //Add values to db
+  $("#add-train").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = $("#train-time-input").val().trim();
+    var frequency = $("#train-frequency-input").val().trim();
+
+    database.ref().push({
+        name: trainName,
+        destination: destination,
+        time: time,
+        freqency: frequency
+    });
+
+    // database.ref().on("value", function(){
+    //     var dbObj = database.val();
+    //     var moment = moment(dbObj.time,"HH:mm");
+
+    // })
+
+  });
 
   //Calculating freq with moment JS.
   //convert the time and then diff the two in min.
